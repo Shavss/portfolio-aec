92112ef43c8d15f0d5ec9a83fc617298a6dded24
Kacper <119957961+Shavss@users.noreply.github.com>
2026-04-19 20:09:17 +0100

    fixed bus

--- app/layout.js
+++ app/layout.js
@@ -25,18 +25,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${spaceMono.variable} ${workSans.variable}`}>
       <head>
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-CKL34G5380"
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-CKL34G5380');
-          `}
-        </Script>
       </head>
       <body>{children}</body>
     </html>
